69bf46fe12c11ff982042e70b8e8b35fe5c54a94
Liu Rui <lovearuis@gmail.com>
2025-10-31 17:32:33 +0800

    feat: 支持联合索引注解，同时改进单元测试写法

--- muyun-database-core/src/main/java/net/ximatai/muyun/database/core/annotation/CompositeIndex.java
+++ muyun-database-core/src/main/java/net/ximatai/muyun/database/core/annotation/CompositeIndex.java
@@ -1,27 +1,15 @@
 package net.ximatai.muyun.database.core.annotation;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Repeatable;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @Repeatable(CompositeIndexes.class)
 public @interface CompositeIndex {
-    /**
-     * 组成联合索引的字段名数组
-     */
+
     String[] columns();
 
-    /**
-     * 索引名称（可选）
-     */
     String name() default "";
 
-    /**
-     * 是否唯一索引
-     */
     boolean unique() default false;
 }
